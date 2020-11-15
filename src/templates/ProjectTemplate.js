6d755f7ee6ba1b78d17222724c4a7273fd5a2555
John Rock <johnmrock.jr@gmail.com>
2020-11-15 15:44:43 -0500

    wip

--- src/templates/ProjectTemplate.js
+++ src/templates/ProjectTemplate.js
@@ -5,6 +5,7 @@ import PageTransition from "gatsby-v2-plugin-page-transitions"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FiChevronsLeft } from "react-icons/fi"
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -18,19 +19,33 @@ export default function Template({
         <div className="project-post-container">
           <div className="project-post">
             <h1>{frontmatter.title}</h1>
-            <div className="project-bar">{frontmatter.date}</div>
-            <div
-              className="project-post-content"
-              dangerouslySetInnerHTML={{ __html: html }}
-            />
-          </div>
-          <div className="sub-btns">
-            <Link className="primary-cta" to="/projects/">
-              <span className="back-btn"> 
-                <FiChevronsLeft />
-              </span>
-              Back
-            </Link>
+            <div class="project-wrapper">
+
+              <div
+                className="project-post-content"
+                dangerouslySetInnerHTML={{ __html: html }}
+              />
+                            <div className="btn-container">
+              <a className="primary-cta" href={frontmatter.url} target="_blank">
+                <FaGithub /> View Project
+              </a>
+              <a
+                className="primary-cta"
+                href={frontmatter.source}
+                target="_blank"
+              >
+                <FaExternalLinkAlt /> View Source Code
+              </a>
+              </div>
+              <div className="sub-btns">
+                <Link className="primary-cta" to="/projects/">
+                  <span className="back-btn">
+                    <FiChevronsLeft />
+                  </span>
+                  Back
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
       </PageTransition>
@@ -45,6 +60,8 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         slug
+        url
+        source
         title
       }
     }
